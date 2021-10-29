2e30f3a9ce22f84d458c40fda65f739080e89feb
gzsdbb <gzsdbb@163.com>
2021-10-29 10:03:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+
+# 修改设置
+sed -i 's/192.168.1.1/192.168.1.200/g' package/base-files/files/bin/config_generate
